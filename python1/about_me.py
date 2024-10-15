3501894b30e1c21f0ec3d5d1bf4a3570676e8b37
pfb2024 <pfb2024@info14.cshl.edu>
2024-10-15 11:20:47 -0400

    added the sys.args to the end

--- python1/about_me.py
+++ python1/about_me.py
@@ -5,3 +5,8 @@ print( "My favorite color: chartruse")
 activity = 'skiing'
 print(f'My favorite activity: {activity:>10}')
 print('My favorite animal: gila monster')
+name = sys.argv[1]
+color = sys.argv[2]
+activity2 = sys.argv[3]
+animal = sys.argv[4]
+print(name, color, activity2, animal)

cb5ed2d833d56bace749ccff30c62560c2b39f5a
pfb2024 <pfb2024@info14.cshl.edu>
2024-10-15 11:17:29 -0400

    finished basic file

---
+++ python1/about_me.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+import sys
+print("My name: Henry")
+print( "My favorite color: chartruse")
+activity = 'skiing'
+print(f'My favorite activity: {activity:>10}')
+print('My favorite animal: gila monster')

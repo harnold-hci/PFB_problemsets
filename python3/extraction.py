e8bea150c3903e80c88bf76a46e9f5c535b43b7b
pfb2024 <pfb2024@info14.cshl.edu>
2024-10-16 17:08:30 -0400

    adding python3 extraction

--- python3/extraction.py
+++ python3/extraction.py
@@ -7,5 +7,14 @@ pos2 = int(sys.argv[3])
 
 dna = input.upper()
 
+extract = dna[(pos1-1):pos2]
+
 print(f'DNA extraction from position {pos1} to position {pos2}:')
-print(dna[(pos1-1):pos2])
+print(extract)
+
+g_count = extract.count('G')
+print(f'G count: {g_count}')
+
+
+
+
